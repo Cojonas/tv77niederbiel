0e76e357bb93131d233a7c67d29ff67671b844e1
jonas <jonas.strunz@zdh.thm.de>
2019-08-21 02:44:45 +0200

    Deploy Next.js to gh-pages

--- out/_next/static/chunks/styles.js
+++ out/_next/static/chunks/styles.js
@@ -29,7 +29,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1566346728725");
+            injectCss(link, link.href.split("?")[0] + "?unix=1566348081378");
           });
       }
     }
