20982fb9c500152f839ac6395b0688c03ee8d5cb
Cojonas <Cojonas@github.com>
2019-08-31 22:11:21 +0200

    Deploy Next.js to gh-pages

--- _next/static/chunks/styles.js
+++ _next/static/chunks/styles.js
@@ -29,7 +29,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1567269561472");
+            injectCss(link, link.href.split("?")[0] + "?unix=1567281054232");
           });
       }
     }
@@ -66,7 +66,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1567268986537");
+            injectCss(link, link.href.split("?")[0] + "?unix=1567281054600");
           });
       }
     }
@@ -103,7 +103,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1567270121089");
+            injectCss(link, link.href.split("?")[0] + "?unix=1567281054239");
           });
       }
     }
