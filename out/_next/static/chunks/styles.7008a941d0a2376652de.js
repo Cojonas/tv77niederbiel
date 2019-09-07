fba13f3167adf9ed0fd6ee1ab29eb26763e91a06
jonas <jonas.strunz@zdh.thm.de>
2019-09-07 11:39:48 +0200

    Deploy Next.js to gh-pages

---
+++ out/_next/static/chunks/styles.7008a941d0a2376652de.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{a6qw:function(w,n,o){}}]);
--- out/_next/static/runtime/main.js
+++ out/_next/static/runtime/main.js
@@ -6313,7 +6313,7 @@ function connect(options) {
       error.name = err.name;
       error.stack = err.stack; // __NEXT_DIST_DIR is provided by webpack
 
-      (0, _sourceMapSupport.rewriteStacktrace)(error, "/home/cojonas/Projects/tv77niederbiel/.next");
+      (0, _sourceMapSupport.rewriteStacktrace)(error, "/home/cojoonas/Documents/Projects/tv77/.next");
       return error;
     }
   };
